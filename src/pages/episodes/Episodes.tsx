d4ae8c97cebc405e7b386f55e0143d1345c9e818
Razmik <razmik.unanyan@mail.ru>
2025-02-26 15:47:28 -0600

    remove log

--- src/pages/episodes/Episodes.tsx
+++ src/pages/episodes/Episodes.tsx
@@ -10,7 +10,7 @@ import {useEpisodes} from "./query";
 
 export const Episodes: FC<IEpisodesProps> = () => {
   const { data: episodes, isLoading, error } = useEpisodes();
-  console.log('isLoading', isLoading)
+
   return (
     <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
     <div className={styles.product_header}>
